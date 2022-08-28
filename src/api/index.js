9529ae94e79331119349076eb662f36c02a157d5
NikaAbesalashvili <nikolozi.abesalashvili.1@btu.edu.ge>
2022-08-28 21:11:30 +0400

    fixed some bug

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: 'https://pcfy.redberryinternship.ge/api' });
 
-const fetchTeams = () =>  API.get('/teams');
-const fetchBrands = () => API.get('/brands');
-const fetchCPUs = () => API.get('/cpus');
-const fetchPositions = () => API.get('/positions');
+export const fetchTeams = () =>  API.get('/teams');
+export const fetchBrands = () => API.get('/brands');
+export const fetchCPUs = () => API.get('/cpus');
+export const fetchPositions = () => API.get('/positions');
 
 // Laptops endpoints
 export const createLaptop = (laptopData) => API.post('/laptop/create', laptopData);
